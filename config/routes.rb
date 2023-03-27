72bc3237fa69977dba87a515880463fb68669535
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-03-27 23:40:45 +0200

    Add the :as attributes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/:username", to: "users#show", as: "user_path"
+  get "/:username", to: "users#show", as: "user"
 
   get 'machines/new'
   post 'machines/create'
   delete 'machines/destroy'
-  get ':username/:name', to: "machines#show"
-  get ':username/:name/edit', to: "machines#edit"
-  patch ':username/:name/update', to: "machines#update"
+  get ':username/:name', to: "machines#show", as: "machine"
+  get ':username/:name/edit', to: "machines#edit", as: "edit_machine"
+  patch ':username/:name/update', to: "machines#update", as: "update_machine"
 
   # Defines the root path route ("/")
   root "home#index"
