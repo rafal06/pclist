506099c9ec6645d224bd4214befa9d5081b083f3
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-04-18 08:59:58 +0200

    Fix dots in username and machine name

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/:username", to: "users#show", as: "user"
+  get "/:username", to: "users#show", as: "user", username: /[^\/]+/
 
   get 'machines/new'
   post 'machines', to: "machines#create"
   delete 'machines/:id', to: "machines#destroy", as: "destroy_machine"
-  get ':username/:name', to: "machines#show", as: "machine"
-  get ':username/:name/edit', to: "machines#edit", as: "machines_edit"
-  patch ':username/:name', to: "machines#update", as: "machines_update"
+  get ':username/:name', to: "machines#show", as: "machine", username: /[^\/]+/, name: /[^\/]+/
+  get ':username/:name/edit', to: "machines#edit", as: "machines_edit", username: /[^\/]+/, name: /[^\/]+/
+  patch ':username/:name', to: "machines#update", as: "machines_update", username: /[^\/]+/, name: /[^\/]+/
 
   # Defines the root path route ("/")
   root "home#index"
