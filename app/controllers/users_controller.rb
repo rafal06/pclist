730847a153d076ed6e2082e54be2ac1b59d4e45c
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-04-18 09:46:24 +0200

    Fix displaying 404 when a user is not found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:username])
     unless @user
       render file: "#{Rails.root}/public/404.html", status: :not_found
+      return
     end
 
     @machines = @user.machines.all
