48449e78440719e423683db631831e761b72e828
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-09-03 20:55:53 +0200

    Implement a homepage for signed in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
   def index
+    unless user_signed_in?
+      # I'll implement a landing page later
+      redirect_to user_session_path
+      return
+    end
+
+    @collection = current_user.machines
+    @explore = Machine.where(user: current_user)
+                      .invert_where
+                      .order("RANDOM()")
+                      .limit(6)
+
     render
   end
 end
